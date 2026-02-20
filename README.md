0e5f712e19e89defd781153247e093bf5ad59df6
Jasper De Jonghe <Jasper.De.Jonghe.07@gmail.com>
2026-02-20 22:58:19 +0100

    feat: eerste snippets

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# school-templates
+# school-templates README
+
+Dit is de extensie voor de extra code-snippets van het eerste jaar MCT die niet in de officiele extensie zitten.
+
+Mogelijke snippets:
+
+- 1MCT.try_except_finally
+- 1MCT.led_setup
+- 1MCT.button_pullup_setup
---
+++ extension.js
@@ -0,0 +1,31 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    'school-templates.helloWorld',
+    function () {
+      vscode.window.showInformationMessage('Hello VS Code!');
+    },
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
